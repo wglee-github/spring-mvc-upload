8a28c9c93a5141a8fa9ad64a77006c942d1bd093
wglee <amd0223@gmail.com>
2023-07-08 20:48:05 +0900

    섹션 11. 파일 업로드
    1.파일 업로드 소개
    2.프로젝트 생성
    3.서블릿과 파일 업로드1
    4.서블릿과 파일 업로드2

---
+++ src/main/java/hello/upload/SpringMvcUploadApplication.java
@@ -0,0 +1,13 @@
+package hello.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcUploadApplication.class, args);
+	}
+
+}
